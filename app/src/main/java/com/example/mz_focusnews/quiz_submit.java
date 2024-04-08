118435a6b2453afea9e6bcffe803b6b4edd432ad
choiM1ng <furigia06@naver.com>
2024-04-08 21:13:07 +0900

    퀴즈화면, 마이페이지

--- app/src/main/java/com/example/mz_focusnews/quiz_submit.java
+++ app/src/main/java/com/example/mz_focusnews/quiz_submit.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link lastQuiz#newInstance} factory method to
+ * Use the {@link quiz_submit#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class lastQuiz extends Fragment {
+public class quiz_submit extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class lastQuiz extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public lastQuiz() {
+    public quiz_submit() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class lastQuiz extends Fragment {
      * @return A new instance of fragment lastQuiz.
      */
     // TODO: Rename and change types and number of parameters
-    public static lastQuiz newInstance(String param1, String param2) {
-        lastQuiz fragment = new lastQuiz();
+    public static quiz_submit newInstance(String param1, String param2) {
+        quiz_submit fragment = new quiz_submit();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class lastQuiz extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_last_quiz, container, false);
+        return inflater.inflate(R.layout.fragment_quiz_submit, container, false);
     }
 }
