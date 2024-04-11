86a7f4978d93428ca2893ba24d1ce403f8c12a42
romi <tofha1108@naver.com>
2024-04-11 20:56:17 +0900

    .

--- app/src/main/java/com/example/mz_focusnews/ContentFragment.java
+++ app/src/main/java/com/example/mz_focusnews/ContentFragment.java
@@ -7,12 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ContentFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+
+        return view;
     }
 }
