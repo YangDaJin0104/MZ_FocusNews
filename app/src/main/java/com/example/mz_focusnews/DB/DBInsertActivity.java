67716e4fcac4ef290238c582342c1534ed4a3ecf
codd1 <mgs08041@gmail.com>
2024-04-17 06:11:28 +0900

    주석 추가) AWS DB용 가상 서버 사용 방법 추가

--- app/src/main/java/com/example/mz_focusnews/DB/DBInsertActivity.java
+++ app/src/main/java/com/example/mz_focusnews/DB/DBInsertActivity.java
@@ -23,6 +23,57 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
+/*
+        < AWS DB 서버 사용 방법 >
+
+        ★ AWS EC2 "DB" 인스턴스 Elastic IP 주소: 43.201.173.245
+
+        ※ 서버가 중지된 상태 or 서버 CPU 사용률이 100%가 되어 죽은 경우는 접속이 안될 수 있음
+          > AWS 계정 공유 혹은 사용자 추가?해서 인스턴스 종료 후 재시작하면 정상 작동
+
+
+        0. Elastic IP 주소로의 웹 연결 확인
+        > 43.201.173.245/
+        > 위 주소로 입력해서 들어갔을 때 "Ubuntu Apache2 Default Page + It works!" 가 안 뜨면 연결 실패
+
+
+
+        1. 웹 서버 연결 확인 (=웹 서버와 DB 연결이 성공적으로 이루어짐)
+            > 43.201.173.245/DBConnection.php
+            > 위 주소 입력해서 들어갔을 때 아무것도 안 뜨면 연결 성공
+
+        2. DB 테이블의 전체 데이터 확인 (SELECT)
+            > 43.201.173.245/getJson.php
+            > 위 주소 입력해서 들어가면 아래와 같은 결과가 출력됨 (무한로딩, 혹은 404 버그 뜨면 연결 실패)
+        {
+            "coddl": [
+                {
+                    "id": 1,
+                    "name": "test",
+                    "country": "test"
+                },
+                {
+                    "id": 2,
+                    "name": "123",
+                    "country": "dgasdf"
+                },
+
+        3. DB 테이블에 데이터 삽입 (INSERT) - 연결 테스트용
+            > 43.201.173.245/DBInsert.php
+            > 위 주소 입력해서 들어가면 데이터 삽입 가능(임시 UI 제작해둠) (무한로딩, 혹은 404 버그 뜨면 연결 실패)
+
+
+        2, 3번 테이블명 및 컬럼 수정 방법
+            1. 가상 서버 접속
+            2. $ cd /var/www/html		(VS Code로 접속 시 이미 파일이 들어가 있으니 테이블명만 수정하면 됨)
+            3. $ sudo vim getJson.php(혹은 DBInsert.php)
+            4. vim 안에서 내용 수정
+            5. 웹 새로고침하면 변경 사항이 적용될 것임~
+
+        MZ_FocusNews\app\src\main\java\com\example\mz_focusnews\DB\에 참고용으로 .php 파일 올려뒀으니 거기서 수정하고 복붙해도 됩니당~
+*/
+
+
 public class DBInsertActivity extends AppCompatActivity {
 
     private static String IP_ADDRESS = "43.201.173.245";
