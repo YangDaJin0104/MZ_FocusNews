f71b6bc50449685336d2a3122c5156b2b37d0717
romi <tofha1108@naver.com>
2024-04-16 20:34:38 +0900

    로그인/회원가입 구현중

--- app/src/main/java/com/example/mz_focusnews/LoginActivity.java
+++ app/src/main/java/com/example/mz_focusnews/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.mz_focusnews;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edt_id;
+    EditText edt_pw;
+    TextView tv_goToFindPw;
+    TextView tv_goToSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,20 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edt_id = (EditText) findViewById(R.id.et_id);
+        edt_pw = (EditText) findViewById(R.id.et_pw);
+
+        // 비밀번호 찾기 페이지로 이동
+        tv_goToFindPw = (TextView) findViewById(R.id.goToFindPwd);
+        // ...
+
+        // 회원가입 페이지로 이동
+        tv_goToSignup = (TextView) findViewById(R.id.goToSignup);
+        tv_goToSignup.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
