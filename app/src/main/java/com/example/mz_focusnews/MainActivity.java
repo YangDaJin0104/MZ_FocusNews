44427212626e0de12f83054327f8ad91653a5532
choiM1ng <furigia06@naver.com>
2024-04-16 17:16:14 +0900

    Initial

---
+++ app/src/main/java/com/example/mz_focusnews/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mz_focusnews;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 하단 네비게이션 바를 위한 변수
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    ContentFragment contentFragment;
+    CategoryFragment categoryFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup); // xml 파일 에뮬레이터 테스트
+    }
+}
