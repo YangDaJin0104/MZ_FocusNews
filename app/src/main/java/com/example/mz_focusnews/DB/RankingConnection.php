e2925d9dc28993b5021f76584ec5a3f78a01d2f5
codd1 <mgs08041@gmail.com>
2024-05-14 06:26:09 +0900

    Ranking 구현 중 - 랭킹 매긴 후 1, 2, 3등, 사용자의 등수 가져와 저장(DB서버 파일)

--- app/src/main/java/com/example/mz_focusnews/DB/RankingConnection.php
+++ app/src/main/java/com/example/mz_focusnews/DB/RankingConnection.php
@@ -1,9 +1,9 @@
 <?php
 // MySQL 서버 연결 정보 설정
-$servername = "43.201.173.245"; // MySQL 서버 주소
-$username = "coddl"; // MySQL 사용자 이름
-$password = "1234"; // MySQL 비밀번호
-$dbname = "users"; // 사용할 데이터베이스 이름
+$servername = "43.201.173.245";     // MySQL 서버 주소
+$username = "coddl";                // MySQL 사용자 이름
+$password = "1234";                 // MySQL 비밀번호
+$dbname = "users";                  // 사용할 데이터베이스 이름
 
 // MySQL 데이터베이스와 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,11 +13,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// 모든 테이블 데이터를 가져오는 쿼리 작성
-$sql = "SELECT * FROM ranking"; // 테이블 이름은 ranking으로 가정합니다.
+// 랭킹 구하는 쿼리
+$sql = "SET @rank=0;";
+$conn->query($sql);
 
-// 쿼리 실행
+$sql = "SELECT @rank:=@rank+1 AS `rank`, user_id, score FROM ranking ORDER BY score DESC;";
 $result = $conn->query($sql);
+
 
 // 결과를 담을 배열 초기화
 $response = array();
