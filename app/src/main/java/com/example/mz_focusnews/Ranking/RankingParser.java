e3bbe0a945aac21754d68533657588ede9bb8a51
codd1 <mgs08041@gmail.com>
2024-05-14 06:24:20 +0900

    Ranking 구현 중 - 랭킹 매긴 후 1, 2, 3등, 사용자의 등수 가져와 저장

--- app/src/main/java/com/example/mz_focusnews/Ranking/RankingParser.java
+++ app/src/main/java/com/example/mz_focusnews/Ranking/RankingParser.java
@@ -11,25 +11,27 @@ import java.util.List;
 
 public class RankingParser {
     private static final String TAG = "RankingParser";
+    private static final String USER_ID = "user4";      // 현재 앱 사용자(테스트용 하드코딩)
 
     public static List<Ranking> parseRanking(String json) {
         List<Ranking> rankings = new ArrayList<>();
 
         try {
             JSONArray jsonArray = new JSONArray(json);
-
+            
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
 
                 String userId = jsonObject.optString("user_id");
                 int score = jsonObject.optInt("score");
 
-                Log.d(TAG, "*** userId = " + userId + " score = " + score);     // DB로부터 가져온 데이터 확인
+                // Ranking 객체를 생성해 데이터 저장 - 1, 2, 3등, 사용자 랭킹 정보만 리스트에 저장
+                if(i < 3 || userId.equals(USER_ID)){
+                    Ranking ranking = new Ranking(userId, score);
+                    rankings.add(ranking);
 
-                // Ranking 객체를 생성해 데이터 저장
-                Ranking ranking = new Ranking(userId, score);
-
-                rankings.add(ranking);
+                    Log.d(TAG, i+1 + "등: userId = " + ranking.getUserId() + " score = " + ranking.getScore());   // 테스트
+                }
             }
         } catch (JSONException e) {
             e.printStackTrace();
