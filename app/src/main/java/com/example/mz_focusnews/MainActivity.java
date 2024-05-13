1662a62a7d2e26a26e19123366d2f2e19905d51f
dajinYang <didekwls0104@naver.com>
2024-05-13 23:18:27 +0900

    location

--- app/src/main/java/com/example/mz_focusnews/MainActivity.java
+++ app/src/main/java/com/example/mz_focusnews/MainActivity.java
@@ -1,26 +1,38 @@
-/*
 package com.example.mz_focusnews;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-
 import com.example.mz_focusnews.NewsCrawling.*;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private NewsAdapter newsAdapter;
     private NewsScraper newsScraper;
 
+    // 위치정보
+    private LocationAddress locationAddress;
+    private NewsSourceConfig newsSourceConfig;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_item_news); // 액티비티의 메인 레이아웃 파일을 사용하도록 변경
+        setContentView(R.layout.activity_main); // 올바른 메인 레이아웃을 사용하도록 수정
 
         // RecyclerView 설정
         recyclerView = findViewById(R.id.recyclerView);
@@ -33,8 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         // 뉴스 기사 가져오기
         fetchArticles();
+
+        // 위치정보 권한설정
+        if (checkPermission()) {
+            initLocationAddress();
+        } else {
+            requestPermissions();
+        }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void fetchArticles() {
         newsScraper.fetchNews(new NewsScraperCallback() {
             @Override
@@ -51,6 +71,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initLocationAddress() {
+        newsSourceConfig = new NewsSourceConfig();
+        locationAddress = new LocationAddress(this, newsSourceConfig);
+        locationAddress.getAddress(new LocationAddress.AddressCallback() {
+            @Override
+            public void onAddressRetrieved(String address) {
+                System.out.println("Address: " + address);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                System.out.println("Error: " + errorMessage);
+            }
+        });
+    }
+
+    private boolean checkPermission() {
+        int fineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int coarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        return fineLocationPermission == PackageManager.PERMISSION_GRANTED && coarseLocationPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, 100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initLocationAddress();
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -59,4 +116,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-*/
