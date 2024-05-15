79f47fdcdb0905c8c5ec788eab6893e4e4f7c57f
romi <tofha1108@naver.com>
2024-05-16 02:45:49 +0900

    파일 수정

--- app/src/main/java/com/example/mz_focusnews/QuizStartActivity.java
+++ app/src/main/java/com/example/mz_focusnews/QuizStartActivity.java
@@ -14,7 +14,7 @@ public class QuizStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_quiz_start);
+        setContentView(R.layout.fragment_ranking);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/mz_focusnews/quiz_submit.java
+++ app/src/main/java/com/example/mz_focusnews/quiz_submit.java
@@ -59,6 +59,6 @@ public class quiz_submit extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz_submit, container, false);
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
     }
 }
