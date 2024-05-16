1f6d218cbdafeca01905ad3c8cef20f5cdbb8fce
codd1 <mgs08041@gmail.com>
2024-05-17 04:22:12 +0900

    프론트 - 화면에 퀴즈 표시(다음 문제로 넘어가기도 성공)

--- app/src/main/java/com/example/mz_focusnews/Quiz/AnswerChecker.java
+++ app/src/main/java/com/example/mz_focusnews/Quiz/AnswerChecker.java
@@ -1,13 +1,16 @@
 package com.example.mz_focusnews.Quiz;
 
-import android.util.Log;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AnswerChecker {
     private Map<Integer, String> correctAnswers;
+
+    public AnswerChecker(Question quizQuestion) {
+        correctAnswers = new HashMap<>();   // 해시맵을 이용해 출제된 문제ID와 정답 저장
+        correctAnswers.put(quizQuestion.getId(), quizQuestion.getCorrectAnswer());
+    }
 
     public AnswerChecker(List<Question> quizQuestions) {
         correctAnswers = new HashMap<>();   // 해시맵을 이용해 출제된 문제ID와 정답 저장
@@ -18,9 +21,7 @@ public class AnswerChecker {
     }
 
     // 사용자가 입력한 답을 받아 정답과 비교하여 점수를 계산합니다.
-    public int checkAnswers(Map<Integer, String> userAnswers) {
-        int score = 0;              // 총 점수
-        int correctCount = 0;       // 맞춘 문제 갯수
+    public boolean isCorrectAnswer(Map<Integer, String> userAnswers) {
         
         // 각 문제의 답을 비교하면서 정답인 경우 score를 증가시킴
         for (int id : userAnswers.keySet()) {
@@ -29,31 +30,10 @@ public class AnswerChecker {
 
             if (userAnswer.equals(correctAnswer)) {
                 System.out.println("정답입니다!!!");     // 테스트
-                correctCount++;
+                return true;
             }
         }
 
-        // 정답 수에 따라 점수 계산
-        switch (correctCount) {
-            case 1:
-                score = 1;
-                break;
-            case 2:
-                score = 3;
-                break;
-            case 3:
-                score = 5;
-                break;
-            case 4:
-                score = 7;
-                break;
-            case 5:
-                score = 10;
-                break;
-            default:
-                break;
-        }
-
-        return score;
+        return false;
     }
 }
--- app/src/main/java/com/example/mz_focusnews/QuizFragment.java
+++ app/src/main/java/com/example/mz_focusnews/QuizFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mz_focusnews.Quiz.AnswerChecker;
 import com.example.mz_focusnews.Quiz.CSVFileReader;
@@ -38,23 +39,30 @@ public class QuizFragment extends Fragment {
     private static final String SUMMARIZE2 = "한국신용데이터는 소상공인을 대상으로 하는 인터넷전문은행을 만들 계획이며, 소상공인에 대한 자체적인 신용평가 서비스를 제공한다.";  // 요약2
     private static final String SUMMARIZE3 = "컨소시엄은 예비인가 신청 후 금융감독원 및 금융위의 심사를 거쳐 인가를 받으면 6개월 이내에 영업을 개시할 예정이다.";   // 요약3
 
+    // 프론트
     private Button btn_quiz_submit;
     private NavController navController;
+    private TextView question;
+    private TextView score;
+    private Button option1;
+    private Button option2;
+    private Button option3;
+    private Button option4;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-        btn_quiz_submit = view.findViewById(R.id.quiz_submit);
+        question = view.findViewById(R.id.question);
+        score = view.findViewById(R.id.score);
+        option1 = view.findViewById(R.id.option1);
+        option2 = view.findViewById(R.id.option2);
+        option3 = view.findViewById(R.id.option3);
+        option4 = view.findViewById(R.id.option4);
+        btn_quiz_submit = view.findViewById(R.id.next);
 
+        quiz(view);
         // 게임 시작 버튼 클릭 시 퀴즈 화면으로 이동
-        btn_quiz_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController = Navigation.findNavController(view);
-                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
-            }
-        });
+
 
         return view;
     }
@@ -62,36 +70,30 @@ public class QuizFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        quiz();
+        quiz(view);
     }
 
-    public void quiz(){
+    public void quiz(View view){
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8)
         {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
 
+            int score = 0;                 // 사용자가 얻은 점수
+
             CSVFileReader csvFileReader = new CSVFileReader();
             CSVFileWriter csvFileWriter = new CSVFileWriter();
-            Context context = requireContext(); // 컨텍스트 가져오기
+            Context context = requireContext();         // 컨텍스트 가져오기
 
             // quiz_solved.csv 파일 초기화 (테스트용)
             csvFileWriter.clearQuizSolvedCSVFile(context, QUIZ_SOLVED_FILE_NAME);
 
             // CSV 파일을 읽어옴
-            List<String[]> csvData = csvFileReader.readQuizCSVFile(context, QUIZ_FILE_NAME);
-
-            // 읽어온 데이터 출력
-            for (String[] line : csvData) {
-                StringBuilder lineBuilder = new StringBuilder();
-                for (String value : line) {
-                    lineBuilder.append(value).append(", ");
-                }
-                Log.d(TAG, "CSV Read: " + lineBuilder.toString());
-            }
+            csvFileReader.readQuizCSVFile(context, QUIZ_FILE_NAME);
 
             // 퀴즈 출제
+            // 2~5번째 문제: 문제은행 퀴즈 - quiz.csv 파일에 저장된 퀴즈 리스트 중 4문제
             List<Question> quizQuestions = QuestionGenerator.generateQuestions(context, QUIZ_FILE_NAME, QUIZ_SOLVED_FILE_NAME, USER_ID, QUESTION_COUNT);
 
             // 퀴즈 문제 출력
@@ -105,25 +107,114 @@ public class QuizFragment extends Fragment {
                 Log.d(TAG, "Option 4: " + question.getOption4());
             }
 
-            csvFileReader.readQuizSolvedCSVFile(context, QUIZ_SOLVED_FILE_NAME);
-
-            Question todayQuiz = null;      // 오늘의 퀴즈 생성 결과 (JSON 형태)
-            String response;                // 오늘의 퀴즈 생성 여부
-            // 오늘의 퀴즈(ChatGPT 기반 퀴즈)가 제대로 만들어질 때까지 반복
-            while(todayQuiz == null){
-                response = ChatGPTAPI.chatGPT(SUMMARIZE1, SUMMARIZE2, SUMMARIZE3);
-                todayQuiz = QuestionGenerator.generateTodayQuiz(response);
-            }
+            todayQuiz(view, quizQuestions.get(0));
 
             // 문제 출제 및 채점
             AnswerChecker answerChecker = new AnswerChecker(quizQuestions);
             USER_ANSWER.put(quizQuestions.get(0).getId(), quizQuestions.get(0).getCorrectAnswer());        // 테스트용
-            USER_ANSWER.put(quizQuestions.get(1).getId(), quizQuestions.get(1).getCorrectAnswer());        // 테스트용
-            USER_ANSWER.put(quizQuestions.get(2).getId(), quizQuestions.get(2).getCorrectAnswer());        // 테스트용
-            USER_ANSWER.put(quizQuestions.get(3).getId(), quizQuestions.get(3).getCorrectAnswer());        // 테스트용
-            int score = answerChecker.checkAnswers(USER_ANSWER);
-            Log.d(TAG, "Score: " + score);
+            answerChecker.isCorrectAnswer(USER_ANSWER);
+        }
+    }
+
+    // 1번째 문제: 오늘의 퀴즈 - 오늘의 뉴스 기반의 문제 출제
+    public void todayQuiz(View view, Question nextQuiz){
+        Question todayQuiz = null;      // 오늘의 퀴즈 생성 결과 (JSON 형태)
+        String response;                // 오늘의 퀴즈 생성 여부
+
+        // 오늘의 퀴즈(ChatGPT 기반 퀴즈)가 제대로 만들어질 때까지 반복
+        while(todayQuiz == null){
+            response = ChatGPTAPI.chatGPT(SUMMARIZE1, SUMMARIZE2, SUMMARIZE3);
+            todayQuiz = QuestionGenerator.generateTodayQuiz(response);
         }
 
+        setView(view, todayQuiz, 0);
+
+        clickOption(view, todayQuiz);
+
+        AnswerChecker answerChecker = new AnswerChecker(todayQuiz);
+
+        btn_quiz_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController = Navigation.findNavController(view);
+                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
+                if(answerChecker.isCorrectAnswer(USER_ANSWER)){
+                    setView(view, nextQuiz, 1);
+                } else{
+                    Log.d(TAG, "틀렸습니다!");
+                    System.exit(1);
+                }
+            }
+        });
+    }
+
+    private void setView(View view, Question quiz, int count){
+        String str;
+
+        // 문제 내용 설정
+        str = "Q. " + quiz.getQuestion();
+        question.setText(str);
+
+        // 점수 설정
+        switch (count){     // count = 몇 번째 문제인지 (0~3)
+            case 0:
+                str = "(1점)";
+                break;
+            case 1:
+                str = "(3점)";
+                break;
+            case 2:
+                str = "(5점)";
+                break;
+            case 3:
+                str = "(7점)";
+                break;
+            case 4:
+                str = "(10점)";
+                break;
+            default:
+                break;
+        }
+        score.setText(str);
+
+        // 보기 설정
+        option1.setText(quiz.getOption1());
+        option2.setText(quiz.getOption2());
+        option3.setText(quiz.getOption3());
+        option4.setText(quiz.getOption4());
+    }
+
+    private void clickOption(View view, Question todayQuiz){
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "option1 click");
+                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption1());
+            }
+        });
+
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "option2 click");
+                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption2());
+            }
+        });
+
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "option3 click");
+                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption3());
+            }
+        });
+
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "option4 click");
+                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption4());
+            }
+        });
     }
 }
