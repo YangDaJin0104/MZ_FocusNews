664d4790b25758632ef546ef98aba6a10395803f
codd1 <mgs08041@gmail.com>
2024-05-17 19:22:57 +0900

    프론트 - 화면에 퀴즈 표시(5번 문제까지 넘어가기 성공, 틀릴경우 종료)

--- app/src/main/java/com/example/mz_focusnews/Quiz/ChatGPTAPI.java
+++ app/src/main/java/com/example/mz_focusnews/Quiz/ChatGPTAPI.java
@@ -10,7 +10,7 @@ public class ChatGPTAPI {
     private static final String TAG = "ChatGPTAPI";
     private static final String API_KEY = "//";     // 최민경의 API secret key
 
-    public static String chatGPT(String summarize1, String summarize2, String summarize3) {
+    public static String chatGPT(String summarize) {
         String url = "https://api.openai.com/v1/chat/completions";
         String model = "gpt-3.5-turbo";
 
@@ -22,7 +22,7 @@ public class ChatGPTAPI {
             connection.setRequestProperty("Content-Type", "application/json");
 
             // prompt 생성
-            String prompt = summarize1 + summarize2 + summarize3 + "위 내용을 바탕으로 객관식 문제를 새로 만들어줘. 문제 내용, 정답, 선택지1, 선택지2, 선택지3, 선택지4를 question, answer, option1, option2, option3, option4 Json 형태로 보내줘(answer==option1||2||3||4)";
+            String prompt = summarize + "위 내용을 바탕으로 객관식 문제를 새로 만들어줘. 문제 내용, 정답, 선택지1, 선택지2, 선택지3, 선택지4를 question, answer, option1, option2, option3, option4 Json 형태로 보내줘(answer==option1||2||3||4)";
 
             // request body
             String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
--- app/src/main/java/com/example/mz_focusnews/QuizFragment.java
+++ app/src/main/java/com/example/mz_focusnews/QuizFragment.java
@@ -1,18 +1,24 @@
 package com.example.mz_focusnews;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mz_focusnews.Quiz.AnswerChecker;
@@ -35,33 +41,34 @@ public class QuizFragment extends Fragment {
     // 테스트용 데이터
     private static final int USER_ID = 456;             // 현재 앱 사용자(테스트용 하드코딩) - 로그인 후 정보 받아옴
     Map<Integer, String> USER_ANSWER = new HashMap<>();    // 현재 앱 사용자가 입력한 정답
-    private static final String SUMMARIZE1 = "우리은행이 한국신용데이터가 주도하는 인터넷전문은행 컨소시엄에 투자 의사를 밝혔다.";   // 요약1
-    private static final String SUMMARIZE2 = "한국신용데이터는 소상공인을 대상으로 하는 인터넷전문은행을 만들 계획이며, 소상공인에 대한 자체적인 신용평가 서비스를 제공한다.";  // 요약2
-    private static final String SUMMARIZE3 = "컨소시엄은 예비인가 신청 후 금융감독원 및 금융위의 심사를 거쳐 인가를 받으면 6개월 이내에 영업을 개시할 예정이다.";   // 요약3
+    private static final String SUMMARIZE = "우리은행이 한국신용데이터가 주도하는 인터넷전문은행 컨소시엄에 투자 의사를 밝혔다. " +
+            "한국신용데이터는 소상공인을 대상으로 하는 인터넷전문은행을 만들 계획이며, 소상공인에 대한 자체적인 신용평가 서비스를 제공한다." +
+            "컨소시엄은 예비인가 신청 후 금융감독원 및 금융위의 심사를 거쳐 인가를 받으면 6개월 이내에 영업을 개시할 예정이다.";    // 3문장 요약
 
     // 프론트
-    private Button btn_quiz_submit;
     private NavController navController;
-    private TextView question;
-    private TextView score;
-    private Button option1;
-    private Button option2;
-    private Button option3;
-    private Button option4;
+    private TextView text_question, text_score, text_progress;
+    private Button btn_option1, btn_option2, btn_option3, btn_option4, btn_stop, btn_next, btn_complete;
+    private ImageView img_correct, img_incorrect;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-        question = view.findViewById(R.id.question);
-        score = view.findViewById(R.id.score);
-        option1 = view.findViewById(R.id.option1);
-        option2 = view.findViewById(R.id.option2);
-        option3 = view.findViewById(R.id.option3);
-        option4 = view.findViewById(R.id.option4);
-        btn_quiz_submit = view.findViewById(R.id.next);
+        text_question = view.findViewById(R.id.question);
+        text_score = view.findViewById(R.id.score);
+        text_progress = view.findViewById(R.id.progress);
+        btn_option1 = view.findViewById(R.id.option1);
+        btn_option2 = view.findViewById(R.id.option2);
+        btn_option3 = view.findViewById(R.id.option3);
+        btn_option4 = view.findViewById(R.id.option4);
+        btn_stop = view.findViewById(R.id.stop);
+        btn_next = view.findViewById(R.id.next);
+        btn_complete = view.findViewById(R.id.complete);
 
-        quiz(view);
-        // 게임 시작 버튼 클릭 시 퀴즈 화면으로 이동
+        img_correct = view.findViewById(R.id.quiz_correct);
+        img_incorrect = view.findViewById(R.id.quiz_incorrect);
+
+        showQuiz(view);
 
 
         return view;
@@ -70,13 +77,11 @@ public class QuizFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        quiz(view);
     }
 
-    public void quiz(View view){
+    public void showQuiz(View view) {
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8)
-        {
+        if (SDK_INT > 8) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
 
@@ -92,128 +97,294 @@ public class QuizFragment extends Fragment {
             // CSV 파일을 읽어옴
             csvFileReader.readQuizCSVFile(context, QUIZ_FILE_NAME);
 
-            // 퀴즈 출제
-            // 2~5번째 문제: 문제은행 퀴즈 - quiz.csv 파일에 저장된 퀴즈 리스트 중 4문제
+            // 2~5번째 퀴즈 출제: 문제은행 퀴즈 - quiz.csv 파일에 저장된 퀴즈 리스트 중 4문제
             List<Question> quizQuestions = QuestionGenerator.generateQuestions(context, QUIZ_FILE_NAME, QUIZ_SOLVED_FILE_NAME, USER_ID, QUESTION_COUNT);
 
-            // 퀴즈 문제 출력
-            for (Question question : quizQuestions) {
-                Log.d(TAG, "Question ID: " + question.getId());
-                Log.d(TAG, "Question: " + question.getQuestion());
-                Log.d(TAG, "Correct Answer: " + question.getCorrectAnswer());
-                Log.d(TAG, "Option 1: " + question.getOption1());
-                Log.d(TAG, "Option 2: " + question.getOption2());
-                Log.d(TAG, "Option 3: " + question.getOption3());
-                Log.d(TAG, "Option 4: " + question.getOption4());
-            }
-
-            todayQuiz(view, quizQuestions.get(0));
-
-            // 문제 출제 및 채점
-            AnswerChecker answerChecker = new AnswerChecker(quizQuestions);
-            USER_ANSWER.put(quizQuestions.get(0).getId(), quizQuestions.get(0).getCorrectAnswer());        // 테스트용
-            answerChecker.isCorrectAnswer(USER_ANSWER);
+            quiz(view, quizQuestions);
+            
+            // TODO: 사용자 score DB에 update 필요
         }
     }
 
     // 1번째 문제: 오늘의 퀴즈 - 오늘의 뉴스 기반의 문제 출제
-    public void todayQuiz(View view, Question nextQuiz){
+    public void quiz(View view, List<Question> quizList) {
         Question todayQuiz = null;      // 오늘의 퀴즈 생성 결과 (JSON 형태)
         String response;                // 오늘의 퀴즈 생성 여부
 
         // 오늘의 퀴즈(ChatGPT 기반 퀴즈)가 제대로 만들어질 때까지 반복
-        while(todayQuiz == null){
-            response = ChatGPTAPI.chatGPT(SUMMARIZE1, SUMMARIZE2, SUMMARIZE3);
+        while (todayQuiz == null) {
+            response = ChatGPTAPI.chatGPT(SUMMARIZE);
             todayQuiz = QuestionGenerator.generateTodayQuiz(response);
         }
 
-        setView(view, todayQuiz, 0);
+        setQuizView(view, todayQuiz, 1);
 
         clickOption(view, todayQuiz);
 
         AnswerChecker answerChecker = new AnswerChecker(todayQuiz);
 
-        btn_quiz_submit.setOnClickListener(new View.OnClickListener() {
+        btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navController = Navigation.findNavController(view);
                 //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
-                if(answerChecker.isCorrectAnswer(USER_ANSWER)){
-                    setView(view, nextQuiz, 1);
-                } else{
+                if (answerChecker.isCorrectAnswer(USER_ANSWER)) {
+                    setResultView(true);
+                    csvQuiz1(view, quizList);
+                } else {
                     Log.d(TAG, "틀렸습니다!");
-                    System.exit(1);
+                    setResultView(false);
+                    // TODO: '그만' 버튼 클릭 시 fragment_ranking 화면으로 넘어가야 함.
                 }
             }
         });
     }
 
-    private void setView(View view, Question quiz, int count){
-        String str;
+    private void csvQuiz1(View view, List<Question> quizList) {
+        Question current_quiz;
+
+        current_quiz = quizList.get(1);
+        setQuizView(view, current_quiz, 2);
+        clickOption(view, current_quiz);
+
+        AnswerChecker answerChecker = new AnswerChecker(current_quiz);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController = Navigation.findNavController(view);
+                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
+                if (answerChecker.isCorrectAnswer(USER_ANSWER)) {
+                    setResultView(true);
+                    csvQuiz2(view, quizList);
+                } else {
+                    Log.d(TAG, "틀렸습니다!");
+                    setResultView(false);
+                    // TODO: '그만' 버튼 클릭 시 fragment_ranking 화면으로 넘어가야 함.
+                }
+            }
+        });
+    }
+
+    private void csvQuiz2(View view, List<Question> quizList) {
+        Question current_quiz;
+
+        current_quiz = quizList.get(2);
+        setQuizView(view, current_quiz, 3);
+        clickOption(view, current_quiz);
+
+        AnswerChecker answerChecker = new AnswerChecker(current_quiz);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController = Navigation.findNavController(view);
+                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
+                if (answerChecker.isCorrectAnswer(USER_ANSWER)) {
+                    setResultView(true);
+                    csvQuiz3(view, quizList);
+                } else {
+                    Log.d(TAG, "틀렸습니다!");
+                    setResultView(false);
+                    // TODO: '그만' 버튼 클릭 시 fragment_ranking 화면으로 넘어가야 함.
+                }
+            }
+        });
+    }
+
+    private void csvQuiz3(View view, List<Question> quizList) {
+        Question current_quiz;
+
+        current_quiz = quizList.get(3);
+        setQuizView(view, current_quiz, 4);
+        clickOption(view, current_quiz);
+
+        AnswerChecker answerChecker = new AnswerChecker(current_quiz);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController = Navigation.findNavController(view);
+                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
+                if (answerChecker.isCorrectAnswer(USER_ANSWER)) {
+                    setResultView(true);
+                    csvQuiz4(view, quizList);
+                } else {
+                    Log.d(TAG, "틀렸습니다!");
+                    setResultView(false);
+                    // TODO: '그만' 버튼 클릭 시 fragment_ranking 화면으로 넘어가야 함.
+                }
+            }
+        });
+    }
+
+    private void csvQuiz4(View view, List<Question> quizList) {
+        Question current_quiz;
+
+        current_quiz = quizList.get(4);
+        setQuizView(view, current_quiz, 5);
+        clickOption(view, current_quiz);
+
+        AnswerChecker answerChecker = new AnswerChecker(current_quiz);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController = Navigation.findNavController(view);
+                //navController.navigate(R.id.action_rankingFragment_to_quizFragment);  // 수정 필요
+                if (answerChecker.isCorrectAnswer(USER_ANSWER)) {
+                    setResultView(true);
+                } else {
+                    Log.d(TAG, "틀렸습니다!");
+                    setResultView(false);
+                    // TODO: '그만' 버튼 클릭 시 fragment_ranking 화면으로 넘어가야 함.
+                }
+            }
+        });
+    }
+
+    private void setQuizView(View view, Question quiz, int count) {
+        String str_score, str_progress;
 
         // 문제 내용 설정
-        str = "Q. " + quiz.getQuestion();
-        question.setText(str);
+        str_score = "Q. " + quiz.getQuestion();
+        text_question.setText(str_score);
+
+        // 퀴즈 진행도 설정
+        str_progress = "(" + count + "/5)";
+        text_progress.setText(str_progress);
 
         // 점수 설정
-        switch (count){     // count = 몇 번째 문제인지 (0~3)
-            case 0:
-                str = "(1점)";
-                break;
+        switch (count) {     // count = 몇 번째 문제인지
             case 1:
-                str = "(3점)";
+                str_score = "(1점)";
                 break;
             case 2:
-                str = "(5점)";
+                str_score = "(3점)";
                 break;
             case 3:
-                str = "(7점)";
+                str_score = "(5점)";
                 break;
             case 4:
-                str = "(10점)";
+                str_score = "(7점)";
+                break;
+            case 5:
+                str_score = "(10점)";
                 break;
             default:
                 break;
         }
-        score.setText(str);
+        text_score.setText(str_score);
 
         // 보기 설정
-        option1.setText(quiz.getOption1());
-        option2.setText(quiz.getOption2());
-        option3.setText(quiz.getOption3());
-        option4.setText(quiz.getOption4());
+        btn_option1.setText(quiz.getOption1());
+        btn_option2.setText(quiz.getOption2());
+        btn_option3.setText(quiz.getOption3());
+        btn_option4.setText(quiz.getOption4());
     }
 
-    private void clickOption(View view, Question todayQuiz){
-        option1.setOnClickListener(new View.OnClickListener() {
+    private void setResultView(boolean iscorrect) {
+        ObjectAnimator fadeOut;
+
+        if (iscorrect) {
+            img_correct.setVisibility(View.VISIBLE);
+            fadeOut = ObjectAnimator.ofFloat(img_correct, "alpha", 1f, 0f);
+        } else {
+            img_incorrect.setVisibility(View.VISIBLE);
+            fadeOut = ObjectAnimator.ofFloat(img_incorrect, "alpha", 1f, 0f);
+        }
+
+        // 2초 후에 흐릿하게 사라지는 애니메이션을 실행하는 Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                fadeOut.setDuration(1000);      // 1초간 FadeOut
+                fadeOut.start();
+
+                // 애니메이션이 끝난 후 INVISIBLE로 설정
+                fadeOut.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        img_correct.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                    }
+                });
+            }
+        }, 1000); // 2s
+
+        // 틀렸을 경우, '그만','다음' 버튼 비활성화
+        if (!iscorrect) {
+            btn_complete.setVisibility(View.VISIBLE);
+            btn_stop.setVisibility(View.INVISIBLE);
+            btn_next.setVisibility(View.INVISIBLE);
+        } else{
+
+        }
+    }
+
+    private void clickOption(View view, Question quiz) {
+        int greenColorValue = Color.parseColor("#00ff00");
+        int blueColorValue = Color.parseColor("#1876E3");
+
+        ColorStateList green = ColorStateList.valueOf(greenColorValue);
+        ColorStateList blue = ColorStateList.valueOf(blueColorValue);      // 기본
+
+        btn_option1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "option1 click");
-                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption1());
+                btn_option1.setBackgroundTintList(green);
+                btn_option2.setBackgroundTintList(blue);
+                btn_option3.setBackgroundTintList(blue);
+                btn_option4.setBackgroundTintList(blue);
+                USER_ANSWER.put(quiz.getId(), quiz.getOption1());
             }
         });
 
-        option2.setOnClickListener(new View.OnClickListener() {
+        btn_option2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "option2 click");
-                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption2());
+                btn_option1.setBackgroundTintList(blue);
+                btn_option2.setBackgroundTintList(green);
+                btn_option3.setBackgroundTintList(blue);
+                btn_option4.setBackgroundTintList(blue);
+                USER_ANSWER.put(quiz.getId(), quiz.getOption2());
             }
         });
 
-        option3.setOnClickListener(new View.OnClickListener() {
+        btn_option3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "option3 click");
-                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption3());
+                btn_option1.setBackgroundTintList(blue);
+                btn_option2.setBackgroundTintList(blue);
+                btn_option3.setBackgroundTintList(green);
+                btn_option4.setBackgroundTintList(blue);
+                USER_ANSWER.put(quiz.getId(), quiz.getOption3());
             }
         });
 
-        option4.setOnClickListener(new View.OnClickListener() {
+        btn_option4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "option4 click");
-                USER_ANSWER.put(todayQuiz.getId(), todayQuiz.getOption4());
+                btn_option1.setBackgroundTintList(blue);
+                btn_option2.setBackgroundTintList(blue);
+                btn_option3.setBackgroundTintList(blue);
+                btn_option4.setBackgroundTintList(green);
+                USER_ANSWER.put(quiz.getId(), quiz.getOption4());
             }
         });
     }
