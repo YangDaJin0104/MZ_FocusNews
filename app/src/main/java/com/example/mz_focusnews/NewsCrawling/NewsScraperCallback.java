7a3e8aac67c3b8cf1f368cd798759b8a56f47db7
romi <tofha1108@naver.com>
2024-05-24 16:28:09 +0900

    파일 정리

--- app/src/main/java/com/example/mz_focusnews/NewsCrawling/NewsScraperCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.mz_focusnews.NewsCrawling;
-
-import java.util.ArrayList;
-
-public interface NewsScraperCallback {
-    void onSuccess(ArrayList<NewsArticle> articles);
-    void onFailure(Exception e);
-}
