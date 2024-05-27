3ae8fc4190063f9329fa957b3515e9ad051a3481
codd1 <mgs08041@gmail.com>
2024-05-28 05:38:55 +0900

    curl 방식 참고용으로 주석 추가

--- app/src/main/java/com/example/mz_focusnews/NewsDB/ImageGenerator.java
+++ app/src/main/java/com/example/mz_focusnews/NewsDB/ImageGenerator.java
@@ -74,3 +74,17 @@ public class ImageGenerator extends AsyncTask<Void, Void, String> {
         }
     }
 }
+
+/*
+
+curl https://api.openai.com/v1/images/generations \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{
+    "model": "dall-e-3",
+    "prompt": "a white siamese cat",
+    "n": 1,
+    "size": "1024x1024"
+  }'
+
+ */
